b34242f9e24566e14aee15569357949129311051
Madison <madisonchan17@gmail.com>
2023-12-23 16:16:59 -0500

    JS functionality for likes and video

--- script.js
+++ script.js
@@ -12,3 +12,33 @@ function locationChange(element) {
     var location = element.value;
     alert("Location changed to " + location + ".");
 }
+
+var count1 = 12;
+var count2 = 24;
+var count3 = 33;
+
+function likeNum1 () {
+    var num1vote = document.querySelector("#num1");
+    count1++;
+    num1vote.innerText = count1 + " votes"
+}
+
+function likeNum2 () {
+    var num2vote = document.querySelector("#num2");
+    count2++;
+    num2vote.innerText = count2 + " votes"   
+}
+
+function likeNum3 () {
+    var num3vote = document.querySelector("#num3");
+    count3++;
+    num3vote.innerText = count3 + " votes"   
+}
+
+function playVideo(element) {
+    element.play();
+}
+
+function pauseVideo(element) {
+    element.pause();
+}
